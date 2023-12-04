0db876fe905dffeb6cc7587ba73033c548e85f6d
Juan Campos <45372981+JuanCampbsi@users.noreply.github.com>
2023-12-04 18:56:42 -0300

    feat: code finish

--- README.md
+++ README.md
@@ -1,10 +1,34 @@
-# Aprenda Kotlin Com Exemplos: Desafio de Projeto (Lab)
+![Kotlin](https://img.shields.io/badge/kotlin-%237F52FF.svg?style=for-the-badge&logo=kotlin&logoColor=white)
 
-Desafio de Projeto criado para avaliação do conteúdo técnico explorado no repositório [aprenda-kotlin-com-exemplos](https://github.com/digitalinnovationone/aprenda-kotlin-com-exemplos). **Nesse contexto, iremos abstrair o seguinte domínio de aplicação:**
+Desafio de Projeto (Lab)
 
-**A [DIO](https://web.dio.me) possui `Formacoes` incríveis que têm como objetivo oferecer um conjunto de `ConteudosEducacionais` voltados para uma stack tecnológica específica, preparando profissionais de TI para o mercado de trabalho. `Formacoes` possuem algumas características importantes, como `nome`, `nivel` e seus respectivos `conteudosEducacionais`. Além disso, tais experiências educacionais têm um comportamento relevante ao nosso domínio, definido pela capacidade de `matricular` um ou mais `Alunos`.**
+### Sobre o Projeto
+Este projeto é um desafio de programação criado como parte do repositório "Aprenda Kotlin com Exemplos". O objetivo deste desafio é aplicar e avaliar os conhecimentos técnicos adquiridos através dos exemplos e tutoriais disponíveis no repositório.
 
+Neste projeto, abordamos um domínio de aplicação específico relacionado ao campo da educação tecnológica. A DIO (Digital Innovation One) oferece uma gama de Formacoes que são cuidadosamente projetadas para ensinar conjuntos de habilidades específicas na área de tecnologia da informação. Cada Formacao é composta de vários ConteudosEducacionais que juntos preparam os profissionais para os desafios do mercado de trabalho atual.
 
-```kotlin
-TODO("Crie uma solução em Koltin abstraindo esse domínio. O arquivo [desafio.kt] te ajudará 😉")
+Funcionalidades
+O sistema modelado neste projeto apresenta as seguintes funcionalidades chave:
+
+Gestão de Formações: Criação e gerenciamento de formações educacionais, incluindo atributos como nome e nível.
+Conteúdos Educacionais: Cada formação possui uma série de conteúdos educacionais que compõem o currículo de aprendizado.
+Matrícula de Alunos: Capacidade de matricular alunos nas formações, simulando um ambiente educacional real.
+
+### ⌨ Clone
+
+Para utilizá-lo, é necessário clonar o repositório.
+
+```bash
+    %sh 
+    git clone https://github.com/JuanCampbsi/aprenda-kotlin-com-exemplos-lab
 ```
+
+### ⌨ play.kotlinlang.org
+Para rodar o projeto direto no playground do kotlin basta clicar no link abaixo.
+
+-   [PlayGround Kotlinlang](https://pl.kotl.in/dBT6SHI3a) - version JVM 1.9.20
+
+
+👨‍💻 **Author** 💻
+
+Developed by [_Juan Campos_](https://www.linkedin.com/in/juancampos-ferreira/)
--- desafio.kt
+++ desafio.kt
@@ -1,22 +1,88 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-
-
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+data class Usuario(val nome: String, val id: Int) {
+     init {
+        require(nome.isNotBlank()) { "Nome do usuário não pode estar vazio." }
+        require(id > 0) { "Id do usuário deve ser positivo e maior que zero." }
     }
 }
 
-fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivel: Nivel) {
+      init {
+        require(duracao > 0) { "Duração deve ser positiva." }
+    }
+}
+
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
+    private val inscritos = mutableListOf<Usuario>()
+    
+    val totalDuracao by lazy { conteudos.sumOf { it.duracao } }
+
+    fun matricular(usuario: Usuario) {       
+        require(!inscritos.any { it.id == usuario.id }) { "Usuário com id ${usuario.id} já está matriculado." }        
+        inscritos.add(usuario)        
+    }
+    
+    fun getAllFormacaoData() {
+        println("--------- Formação ($nome) ---------\n") 
+        
+        conteudos.forEach { item -> 
+            println("Conteudo Educacional: ${item.nome} | Duração de ${item.duracao} horas | Nível: ${item.nivel} ")
+        }
+        
+       println("\nTotal de Duração: ${this.totalDuracao} horas")           
+        
+       println("\n--------- Alunos Matriculados ---------\n") 
+       
+       println("Nome  |  id") 
+       println("____     __\n") 
+       inscritos.forEach { item -> 
+            println("${item.nome} | ${item.id}")
+            
+        }
+        
+    }
+}
+
+fun main() {  	
+   try {
+       val conteudo = mutableListOf<ConteudoEducacional>(
+        ConteudoEducacional(nome = "Kotlin", duracao = 100, Nivel.DIFICIL),
+        ConteudoEducacional(nome = "Java", duracao = 50, Nivel.BASICO),
+        ConteudoEducacional(nome = "Métodologias Ágeis", duracao = 40, Nivel.INTERMEDIARIO)
+		)       
+
+       val formacao = Formacao(nome="Bootcamp NTT", conteudo).apply {
+           listOf(
+                Usuario(nome = "Juan D.", id = 1),
+                Usuario(nome = "Juli F.", id = 2),
+                Usuario(nome = "Josi F.", id = 3)
+           ).forEach(this::matricular)   
+           
+       }                  
+       
+       formacao.getAllFormacaoData() 
+       
+    } catch (e: Exception) {
+        println("Erro inesperado: ${e.message}")
+    }     
+
+    /* Output
+       ** --------- Formação (Bootcamp NTT) ---------
+
+       ** Conteudo Educacional: Kotlin | Duração de 100 horas | Nível: DIFICIL 
+       ** Conteudo Educacional: Java | Duração de 50 horas | Nível: BASICO 
+       ** Conteudo Educacional: Métodologias Ágeis | Duração de 40 horas | Nível: INTERMEDIARIO 
+
+       **  Total de Duração: 190 horas
+
+       ** --------- Alunos Matriculados ---------
+
+       ** Nome  |  id
+       ** ____     __
+
+       ** Juan D. | 1
+       ** Juli F. | 2
+       ** Josi F. | 3
+    */  
 }
